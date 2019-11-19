67bccb2768653584ffda52a33142d734198cb0fc
Moyu <wnm1503303791@gmail.com>
2019-11-19 19:12:06 +0800

    2019-11-19

--- neural-style-tz/run.sh
+++ neural-style-tz/run.sh
@@ -1,6 +1,2 @@
 
-source activate tensorflow-gpu1
-
-CUDA_VISIBLE_DEVICES=1 python neural_style.py --content examples/concert.jpg --styles s/starry_night_google.jpg --output r2/concert1.jpg
-CUDA_VISIBLE_DEVICES=1 python neural_style.py --content examples/concert.jpg --styles s/the_scream.jpg --output r2/concert2.jpg
-CUDA_VISIBLE_DEVICES=1 python neural_style.py --content examples/concert.jpg --styles s/woman-with-hat-matisse.jpg --output r2/concert3.jpg
+CUDA_VISIBLE_DEVICES=1 python neural_style.py --content content/nju-cs.jpg --styles style/1.jpg --output result/nju-cs-1.jpg
