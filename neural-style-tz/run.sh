4a863af3704fb32c73f97e57bb52d6a588d079f0
Moyu <wnm1503303791@gmail.com>
2019-11-20 16:12:34 +0800

    2019-11-20

--- neural-style-tz/run.sh
+++ neural-style-tz/run.sh
@@ -1,4 +1,4 @@
 #Intel(R) Xeon(R) Gold 6140 CPU @ 2.30GHz * 2, 36 cores
 #Tesla P100-PCIE-16GB * 1 (UUID: GPU-ae5cde47-bf7f-a6c6-8a68-8a3c96b2dadf)
 
-CUDA_VISIBLE_DEVICES=1 python neural_style.py --content content/nju-cs-1.jpg --styles style/1.jpg --output result/result-nju-cs-1.jpg
+CUDA_VISIBLE_DEVICES=1 python neural_style.py --content content/nju-1.jpg --styles style/1.jpg --output result/result-nju-1.jpg
